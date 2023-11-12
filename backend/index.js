9c0b193a8f95f3b8b58e9fbaf93be6b79af90186
T1t45 <titasbartulis@gmail.com>
2023-11-12 18:04:03 +0200

    Post a book and get all books with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,63 @@
-import express from "express";
+import express, { request } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
-app.get('/', (request, response) => {
+// Middleware for parsing request body
+app.use(express.json());
+
+app.get("/", (request, response) => {
   console.log(request);
-  return response.status(234).send('Welcome to MERN Stack Tutorial');
+  return response.status(234).send("Welcome to MERN Stack Tutorial");
+});
+
+// Route for Save a new Book
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Get All Books from database
+
+app.get('/books', async (request, response) => {
+  try {
+    const books = await Book.find({});
+    return response.status(200).json({
+      count: books.length,
+      data: books
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
   .connect(mongoDBURL)
   .then(() => {
-    console.log('App connected to database');
+    console.log("App connected to database");
     app.listen(PORT, () => {
       console.log(`App is listening to port: ${PORT}`);
     });
