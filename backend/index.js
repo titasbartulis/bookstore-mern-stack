b99948fd116be6823d14e7c29221e3307236adac
T1t45 <titasbartulis@gmail.com>
2023-11-12 18:43:05 +0200

    Management of CORS policy

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,18 @@ const app = express();
 // Middleware for parsing request body
 app.use(express.json());
 
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+//app.use(cors());
+// Option 2: Allow Custom Origins
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
+
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to MERN Stack Tutorial");
