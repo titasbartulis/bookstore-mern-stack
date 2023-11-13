992d23d1155616afdad5bbdfec0e25064e7d275e
T1t45 <titasbartulis@gmail.com>
2023-11-13 11:14:11 +0200

    Show Book Details in React

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
   return (
     <div className="p-4">
       <div className="flex justify-between items-center">
-        <h1 className="text-3x1 my-8">Books List</h1>
+        <h1 className="text-4xl my-8">Books List</h1>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-sky-800 text-4x1" />
         </Link>
