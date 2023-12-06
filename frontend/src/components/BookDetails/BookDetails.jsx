d8655bd5c77fda11a05bd4f1f22f783855798341
T1t45 <titasbartulis@gmail.com>
2023-12-06 18:23:01 +0200

    Integrated Google Books API

--- frontend/src/components/BookDetails/BookDetails.jsx
+++ frontend/src/components/BookDetails/BookDetails.jsx
@@ -1,42 +1,60 @@
 import "./BookDetails.css";
-import React, {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Loading from "../Loader/Loader";
 import coverImg from "../../images/cover_not_found.jpg";
-import {FaArrowLeft} from "react-icons/fa";
-import { useNavigate } from 'react-router-dom';
+import { FaArrowLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const URL = "https://openlibrary.org/works/";
 
 const BookDetails = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const [loading, setLoading] = useState(false);
   const [book, setBook] = useState(null);
+  const [isbn, setIsbn] = useState("");
   const navigate = useNavigate();
+  const VITE_GOOGLE_API_KEY = `${import.meta.env.VITE_GOOGLE_API_KEY}`;
+  const googleBooksApiEndpoint = `https://www.googleapis.com/books/v1/volumes?q=${book?.title}&key=${VITE_GOOGLE_API_KEY}`;
 
   useEffect(() => {
     setLoading(true);
-    async function getBookDetails(){
-      try{
+    async function getBookDetails() {
+      try {
         const response = await fetch(`${URL}${id}.json`);
         const data = await response.json();
 
-        if(data){
-          const {description, title, covers, subject_places, subject_times, subjects} = data;
+        if (data) {
+          const {
+            description,
+            title,
+            covers,
+            subject_places,
+            subject_times,
+            subjects,
+          } = data;
           const newBook = {
-            description: description ? description.value : "No description found",
+            description: description
+              ? description.value
+              : "No description found",
             title: title,
-            cover_img: covers ? `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg` : coverImg,
-            subject_places: subject_places ? subject_places.join(", ") : "No subject places found",
-            subject_times : subject_times ? subject_times.join(", ") : "No subject times found",
-            subjects: subjects ? subjects.join(", ") : "No subjects found"
+            cover_img: covers
+              ? `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg`
+              : coverImg,
+            subject_places: subject_places
+              ? subject_places.join(", ")
+              : "No subject places found",
+            subject_times: subject_times
+              ? subject_times.join(", ")
+              : "No subject times found",
+            subjects: subjects ? subjects.join(", ") : "No subjects found",
           };
           setBook(newBook);
         } else {
           setBook(null);
         }
         setLoading(false);
-      } catch(error){
+      } catch (error) {
         console.log(error);
         setLoading(false);
       }
@@ -44,44 +62,84 @@ const BookDetails = () => {
     getBookDetails();
   }, [id]);
 
-  if(loading) return <Loading />;
+  const findISBN = async () => {
+    try {
+      const response = await fetch(googleBooksApiEndpoint);
+      const data = await response.json();
+      let isbn_13 = "";
+
+      data.items.forEach((item) => {
+        if (
+          item?.volumeInfo?.title === book?.title &&
+          item?.volumeInfo?.industryIdentifiers?.some(
+            (identifier) => identifier.type === "ISBN_13"
+          )
+        ) {
+          const isbnIdentifier = item.volumeInfo.industryIdentifiers.find(
+            (identifier) => identifier.type === "ISBN_13"
+          );
+          isbn_13 = isbnIdentifier.identifier;
+        }
+      });
+      setIsbn(isbn_13);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (loading) return <Loading />;
 
   return (
-    <section className='book-details'>
-      <div className='container'>
-        <button type='button' className='flex flex-ai-c flex-c back-btn' onClick={() => navigate("/home_bookhub/book")}>
-          <FaArrowLeft size = {22} />
-          <span className='fs-18 fw-6'>Go Back</span>
+    <section className="book-details">
+      <div className="container">
+        <button
+          type="button"
+          className="flex flex-ai-c flex-c back-btn"
+          onClick={() => navigate("/home_bookhub/book")}
+        >
+          <FaArrowLeft size={22} />
+          <span className="fs-18 fw-6">Go Back</span>
         </button>
 
-        <div className='book-details-content grid'>
-          <div className='book-details-img'>
-            <img src = {book?.cover_img} alt = "cover img" />
+        <div className="book-details-content grid">
+          <div className="book-details-img">
+            <img src={book?.cover_img} alt="cover img" />
           </div>
-          <div className='book-details-info'>
-            <div className='book-details-item title'>
-              <span className='fw-6 fs-24'>{book?.title}</span>
+          <div className="book-details-info">
+            <div className="flex flex-ai-c">
+              <button
+                className="bg-black text-white rounded-lg px-12 py-6 text-capitalize fs-16"
+                onClick={findISBN}
+              >
+                Find ISBN?
+              </button>
+              {isbn && 
+                <p className="ml-12 fs-24">The ISBN - {isbn}</p>
+              }
             </div>
-            <div className='book-details-item description'>
+            <div className="book-details-item title">
+              <span className="fw-6 fs-24">{book?.title}</span>
+            </div>
+            <div className="book-details-item description">
               <span>{book?.description}</span>
             </div>
-            <div className='book-details-item'>
-              <span className='fw-6'>Subject Places: </span>
-              <span className='text-italic'>{book?.subject_places}</span>
+            <div className="book-details-item">
+              <span className="fw-6">Subject Places: </span>
+              <span className="text-italic">{book?.subject_places}</span>
             </div>
-            <div className='book-details-item'>
-              <span className='fw-6'>Subject Times: </span>
-              <span className='text-italic'>{book?.subject_times}</span>
+            <div className="book-details-item">
+              <span className="fw-6">Subject Times: </span>
+              <span className="text-italic">{book?.subject_times}</span>
             </div>
-            <div className='book-details-item'>
-              <span className='fw-6'>Subjects: </span>
+            <div className="book-details-item">
+              <span className="fw-6">Subjects: </span>
               <span>{book?.subjects}</span>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default BookDetails
+export default BookDetails;
